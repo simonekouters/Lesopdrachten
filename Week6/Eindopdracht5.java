bcde76dd4b815e1f0b4cc89a1d1c946e42560ad8
simonekouters <140211850+simonekouters@users.noreply.github.com>
2024-02-26 22:06:54 +0100

    Redid the assignment

--- Week6/Eindopdracht5.java
+++ Week6/Eindopdracht5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.LinkedHashMap;
 
 public class Eindopdracht5 {
     public static void main(String[] args) {
@@ -10,9 +11,7 @@ public class Eindopdracht5 {
         System.out.println("Aantal woorden: " + countWords(text));
         System.out.println("Aantal klinkers: " + countVowels(text));
         System.out.println("Palindroom? " + isPalindrome(text));
-        System.out.println();
-        printDiagram(text);
-        
+        printDiagram(text); 
     }
     
     private static int countCharacters(String text) {
@@ -35,7 +34,6 @@ public class Eindopdracht5 {
         return count;
     }
     
-    
     private static boolean isPalindrome(String text) { 
         String textWithoutSpaces = text.replaceAll(" ", "");
         StringBuilder bldr = new StringBuilder(textWithoutSpaces);
@@ -43,19 +41,67 @@ public class Eindopdracht5 {
         return reversed.equals(textWithoutSpaces);    
     }
     
-    private static void printDiagram(String text) {
-        char[] characters = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
-        's','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9', ' '};
+    private static void printDiagram(String text) { 
+        LinkedHashMap<Character, Integer> charactersToPrint = createCharacterMap(text);
+        int maxHeight = maxHeight(charactersToPrint);
         
-        for (char c : characters) {
-            System.out.print(c + " ");
-            for (char ch : text.toCharArray()) {
-                if (ch == c) {
-                    System.out.print("*");
+        StringBuilder builder = new StringBuilder();
+        
+        for (int i = maxHeight; i > 0; i--) {
+            for (Integer charCount : charactersToPrint.values()) {
+                if (i <= charCount) {
+                    builder.append("* ");
+                } else {
+                    builder.append("  ");
                 }
             }
-            System.out.println();
-        } 
+            builder.append("\n");
+        }
+        System.out.print("\n" + builder);
+        
+        for (Character ch : charactersToPrint.keySet()) {
+            System.out.print(ch + " ");
+        }
+    }
+    
+    private static LinkedHashMap<Character, Integer> createCharacterMap(String text) {
+        LinkedHashMap<Character, Integer> charactersToPrint = new LinkedHashMap<>();
+        saveCharacterCounts(97, 123, charactersToPrint, text);
+        saveCharacterCounts(48, 58, charactersToPrint, text);
+        charactersToPrint.put(' ', countSpaces(text));
+        return charactersToPrint;
+    }
+    
+    private static void saveCharacterCounts(int from, int to, LinkedHashMap<Character, Integer> charactersToPrint, String text) {
+        for (int i = from; i < to; i++) { 
+            int count = 0; 
+            for (char ch : text.toCharArray()) {
+                if (ch == (char)i) {
+                    count++;
+                }  
+                charactersToPrint.put((char) i, count);
+            }
+        }   
+    }
+    
+    private static int maxHeight(LinkedHashMap<Character, Integer> charactersToPrint) {
+       int max = 0;
+       for (Integer charCount : charactersToPrint.values()) {
+            if (charCount > max) {
+                max = charCount;
+            }
+        }
+        return max;
+    }
+    
+    private static int countSpaces(String text) {
+        int count = 0;
+        for (char ch : text.toCharArray()) {
+            if (ch == ' ') {
+                count++;
+            }
+        }
+        return count;
     }
 }
 
